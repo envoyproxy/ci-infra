d3a67d95f18b842dbff0684c1d7f654e3e3cc2df
phlax <phlax@users.noreply.github.com>
2023-03-04 02:22:11 +0000

    bazel-remote: Bump version (2.4.0) to fix key mangling (#11)
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- ami-build/scripts/install-bazel-remote.sh
+++ ami-build/scripts/install-bazel-remote.sh
@@ -5,5 +5,5 @@ set -eu -o pipefail
 export ARCH=$(dpkg --print-architecture)
 [[ "${ARCH}" == "amd64" ]] && ARCH="x86_64"
 
-sudo wget -O /usr/local/bin/bazel-remote https://github.com/buchgr/bazel-remote/releases/download/v2.3.9/bazel-remote-2.3.9-linux-${ARCH}
+sudo wget -O /usr/local/bin/bazel-remote https://github.com/buchgr/bazel-remote/releases/download/v2.4.0/bazel-remote-2.4.0-linux-${ARCH}
 sudo chmod 0755 /usr/local/bin/bazel-remote

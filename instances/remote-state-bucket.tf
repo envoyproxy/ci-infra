629c6ff9a084949eb683fe6dea59e6b5b64e6ecf
Jakub Sobon <jakub.sobon@elohim.sk>
2023-04-04 16:06:29 -0400

    Enable versioning on the Terraform state bucket.
    Signed-off-by: Jakub Sobon <jakub.sobon@elohim.sk>

--- instances/remote-state-bucket.tf
+++ instances/remote-state-bucket.tf
@@ -7,6 +7,26 @@ resource "aws_s3_bucket" "remote-state-bucket" {
   }
 }
 
+resource "aws_s3_bucket_versioning" "remote-state-bucket-versioning" {
+  bucket = aws_s3_bucket.remote-state-bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "remote-state-bucket-lifecycle" {
+  bucket = aws_s3_bucket.remote-state-bucket.id
+
+  rule {
+    id = "keep-100-noncurrent-versions-for-a-year"
+    status = "Enabled"
+    noncurrent_version_expiration {
+      newer_noncurrent_versions = 100
+      noncurrent_days = 365
+    }
+  }
+}
+
 terraform {
   backend "s3" {
     bucket = "envoy-build-tf-remote-state-us-east-2"

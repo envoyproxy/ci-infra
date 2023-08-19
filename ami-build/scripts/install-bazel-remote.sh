64daa4166b24031ec18881f30a52f7eb4de93c44
phlax <phlax@users.noreply.github.com>
2023-08-19 08:38:28 +0100

    ami/build: Bump `bazel-remote` -> 2.4.1 (#79)
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- ami-build/scripts/install-bazel-remote.sh
+++ ami-build/scripts/install-bazel-remote.sh
@@ -2,8 +2,10 @@
 
 set -eu -o pipefail
 
+BAZEL_REMOTE_VERSION=2.4.1
+
 export ARCH=$(dpkg --print-architecture)
 [[ "${ARCH}" == "amd64" ]] && ARCH="x86_64"
 
-sudo wget -O /usr/local/bin/bazel-remote https://github.com/buchgr/bazel-remote/releases/download/v2.4.0/bazel-remote-2.4.0-linux-${ARCH}
+sudo wget -O /usr/local/bin/bazel-remote https://github.com/buchgr/bazel-remote/releases/download/v${BAZEL_REMOTE_VERSION}/bazel-remote-${BAZEL_REMOTE_VERSION}-linux-${ARCH}
 sudo chmod 0755 /usr/local/bin/bazel-remote

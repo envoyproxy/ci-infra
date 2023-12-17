2e2024d840c83244f5ed12957010e45c005f9b01
Ryan Northey <ryan@synca.io>
2023-12-17 11:44:22 +0000

    iam: Add Github OIDC provider
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- instances/iam.tf
+++ instances/iam.tf
@@ -93,3 +93,13 @@ resource "aws_iam_role_policy" "cleanup_ec2_perms" {
 
   policy = data.aws_iam_policy_document.lambda_cleanup_ami_permissions.json
 }
+
+### Github OIDC
+
+module "iam_github_oidc_provider" {
+  source    = "terraform-aws-modules/iam/aws//modules/iam-github-oidc-provider"
+
+  tags = {
+    Environment = "Production"
+  }
+}
